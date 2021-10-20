7671887dffdd8439987d8b2f8a0bda0d961b986c
Aynur Erkalaycı <nildem_18_@hotmail.com>
2021-10-20 20:15:09 +0300

    18 Ekim Ders Notu

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log('Merhaba Kodlama.io')
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+console.log(dolarDun)
+//array
+let konutKredileri = ['Konut Kredisi','Emlak Konut Kredisi','Kamu Konut Kredisi','Özel Konut Kreisi']
+
+console.log('<ul>')
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log('<li>'+konutKredileri[i]+'</li>')
+}
+console.log('</ul>')
+
+console.log(konutKredileri)
